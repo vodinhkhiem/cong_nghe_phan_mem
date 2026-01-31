4299cbc2dbcdbdd70455eb3e871f5fff52bbbef6
vodinhkhiem <lamthinang11@gmail.com>
2026-01-31 18:02:46 +0700

    Delete ThietkeAPI-Communication directory

--- ThietkeAPI-Communication/models/conversation.py
+++
@@ -1,8 +0,0 @@
-# models/conversation.py
-# Model hội thoại
-
-class Conversation:
-    def __init__(self, conversation_id, participants):
-        self.conversation_id = conversation_id
-        self.participants = participants
-        self.messages = []
--- ThietkeAPI-Communication/models/whiteboard.py
+++
@@ -1,8 +0,0 @@
-# models/whiteboard.py
-
-class Whiteboard:
-    def __init__(self, project_id, state):
-        # project_id: ID dự án
-        # state: JSON state của bảng trắng
-        self.project_id = project_id
-        self.state = state
