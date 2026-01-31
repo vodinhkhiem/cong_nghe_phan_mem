3adbf9823ff38fbcf3c5bdfb6bcd47fe9334d72e
vodinhkhiem <lamthinang11@gmail.com>
2026-01-31 18:03:02 +0700

    Delete ThietkeAPI-Curriculum Setup directory

--- ThietkeAPI-Curriculum Setup/models/rubric.py
+++
@@ -1,8 +0,0 @@
-# models/rubric.py
-# Mô hình Rubric (tiêu chí chấm điểm)
-
-class Rubric:
-    def __init__(self, rubric_id, title, criteria):
-        self.rubric_id = rubric_id
-        self.title = title
-        self.criteria = criteria
